a47103876cb942f2a8f3b42cb168b7b8247b485f
Nguyen Cao Ky <caoky2003xx@gmail.com>
2023-12-17 01:28:07 +0700

    change input params name

--- path_finding_algo/a_star.py
+++ path_finding_algo/a_star.py
@@ -1,18 +1,21 @@
-import json
 import heapq
+import json
+
 
 class Node:
     def __init__(self, id, x, y):
         self.id = id
         self.pos = (x, y)
 
+
 # dùng để tính khoảng cách giữa 2 node bất kì
 def cost(node1, node2):
     x1, y1 = node1.pos
     x2, y2 = node2.pos
-    return abs(x1 - x2) + abs(y1 - y2) 
+    return abs(x1 - x2) + abs(y1 - y2)
 
-def a_star(start_id, goal_id, NODES_FILE, EDGES_FILE):
+
+def a_star(start, stop, NODES_FILE, EDGES_FILE):
     with open(NODES_FILE) as f:
         nodes = json.load(f)['nodes']
         # tạo dictionary để lưu node
@@ -28,8 +31,8 @@ def a_star(start_id, goal_id, NODES_FILE, EDGES_FILE):
                 graph[u] = {}
             graph[u][v] = edge['length']
 
-    start = node_dict[start_id]
-    goal = node_dict[goal_id]
+    start = node_dict[start]
+    stop = node_dict[stop]
 
     # heap hiệu năng với priority queue tốt hơn list, có pop, push
     open_list = []
@@ -40,32 +43,34 @@ def a_star(start_id, goal_id, NODES_FILE, EDGES_FILE):
     # g_score dùng để lưu chi phí từ nút start đến nút đang xét
     g_score = {start: 0}
     # f_score dùng để ước lượng chi phí tổng cộng của đường đi nếu đi qua nút đó
-    f_score = {start: cost(start, goal)}
+    f_score = {start: cost(start, stop)}
 
-    # ý tưởng của A* same same Dijikstra, với nút kề với nút đang xét và không thuộc before thì chọn nút có g_score + f_score min, sau đó update các nút khác
+    # ý tưởng của A* same same Dijikstra, với nút kề với nút đang xét và không thuộc before thì chọn nút có g_score +
+    # f_score min, sau đó update các nút khác
     while open_list:
         # heap tự động sắp xếp theo thứ tự tăng dần theo f_score nên auto pop thằng thứ nhất để có được khoảng cách min
         current = heapq.heappop(open_list)[1]
 
-        if current == goal:
+        if current == stop:
             data = []
             while current in before:
                 data.append(current)
                 current = before[current]
             data.reverse()
             return data
-        
+
         # cái này same same Dijikstra
         for neighbour in graph[current]:
             current_g_score = g_score[current] + graph[current][neighbour]
             if neighbour not in g_score or current_g_score < g_score[neighbour]:
                 before[neighbour] = current
                 g_score[neighbour] = current_g_score
-                f_score[neighbour] = current_g_score + cost(neighbour, goal)
+                f_score[neighbour] = current_g_score + cost(neighbour, stop)
                 heapq.heappush(open_list, (f_score[neighbour], neighbour))
-    
+
     return None
 
+
 if __name__ == '__main__':
-    path = a_star(8189781671,11112265409, '../input/nodes.json', '../input/edges.json')
+    path = a_star(8189781671, 11112265409, '../input/nodes.json', '../input/edges.json')
     print(path)
